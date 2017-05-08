e6e1a66c5fcce4264a318c49ac1d4b32e83f1797
sdamgren <sandra.damgren@hotmail.com>
2017-05-08 18:30:47 +0200

    Update update.php

--- sandra.dev/view/update.php
+++ sandra.dev/view/update.php
@@ -24,12 +24,8 @@
         background-color: lightgrey;
         width: 100%;
     }
-
 </style>
 <body>
-
-
-
 <form action="/index.php?page=update" method="post">
 <table>
     <thead>
@@ -37,13 +33,9 @@
             <th>Update artist<br><input type="text" name="Artist" value="<?= $music->getArtist() ?>" placeholder="Artist"/></th>
             <th>Update song<br><input type="text" name="Song" value="<?= $music->getSong() ?>" placeholder="Song"/></th>
             <th>Update year<br><input type="text" name="Year" value="<?= $music->getYear() ?>" placeholder="Year"/></th>
-            <th>
-            <th>
-             <input type="hidden" name="id" value="<?= $music->getId() ?>"/>       
+            <input type="hidden" name="id" value="<?= $music->getId() ?>"/>       
             <br><button type="submit">Update artist</button><th>
             <th><br><button><a href="/index.php"/>Go back</button></th>
-        </td>
-
         </tr>
     </thead>
 </table>
